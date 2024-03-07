5fbfac5e27560904a1a54591841094beeb4e6938
Jesemeil <otahaghogho@gmail.com>
2024-03-07 19:22:22 +0100

    Added to UserChat file and avarter

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,13 +1,38 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap";
+import UserChat from "../components/chat/UserChat";
+import { AuthContext } from "../context/AuthContext";
 
 const Chat = () => {
-  const { userChats, isUserChatsLoading, userChatsEror } =
+  const { user } = useContext(AuthContext);
+
+  const { userChats, isUserChatsLoading, userChatsError } =
     useContext(ChatContext);
 
-  console.log("UserChats", userChats);
+  console.log("UserChat", userChats);
 
-  return <>Chat</>;
+  if (userChats === null || isUserChatsLoading) {
+    return <Container>Loading...</Container>;
+  }
+
+  return (
+    <Container>
+      <Stack direction="horizontal" gap={4} className="align-items-start">
+        <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+          {isUserChatsLoading && <p>Loading chats...</p>}
+          {userChats?.map((chat, index) => {
+            return (
+              <div key={index}>
+                <UserChat chat={chat} user={user} />
+              </div>
+            );
+          })}
+        </Stack>
+        <p>ChatBox</p>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Chat;
